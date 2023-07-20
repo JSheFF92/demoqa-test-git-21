c75dc1d78f013e36b4ed375341ba5ee4e71ba60a
Evgenyi <shevchuk291992@gmail.com>
2023-07-20 23:46:47 +0300

    fix code

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,11 +1,9 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
+
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
