41cc22e0ff959f03b9263a161e538b378ca2cc6c
Evgenyi <shevchuk291992@gmail.com>
2023-07-20 23:26:40 +0300

    init

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    static {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @Test
+    void successfulSearchTest() {
+        open("https://google.com/");
+    }
+}
